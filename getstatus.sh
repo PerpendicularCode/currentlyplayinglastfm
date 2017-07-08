249182fc45ea2472eb51f55ead3b58813392c36e
David Craddock <contact@davidcraddock.net>
2017-07-08 13:11:36 +0000

    Reduced poll to 1 instead of 2, set variables properly

--- getstatus.sh
+++ getstatus.sh
@@ -1,10 +1,13 @@
-export old=""
+old=""
 while true; do
-export new="Currently playing: `./getartist.sh` : `./gettrack.sh`"
-if [ "$old" != "$new" ]
-then
-  echo "$new"
-  export old="$new"
-fi
-sleep 3; 
+	response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=davmonster&api_key=$LASTFM_API_KEY&format=json"` 
+	artist=`echo "$response" | jq ".[] | .track | .[0] | .artist " | tr -d "\#" | jq ".text"`
+	track=`echo "$response" | jq ".[] | .track | .[0] | .name"`
+	new="Currently playing: $artist : $track"
+	if [ "$old" != "$new" ]
+		then
+		  echo "$new"
+		  old="$new"
+		fi
+	sleep 3; 
 done
--- gettrack.sh
+++
@@ -1,1 +0,0 @@
-curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=davmonster&api_key=$LASTFM_API_KEY&format=json" | jq ".[] | .track | .[0] | .name "
